cac67638ad3573b264963278b681ec6ce39e5baf
Makarand Patwardhan <mpatwardhan@twilio.com>
2020-07-29 07:40:20 -0700

    test only localtrackpublication

--- scripts/circleci-run-tests.sh
+++ scripts/circleci-run-tests.sh
@@ -54,7 +54,7 @@ then
 else
     # ask circleci to split tests by timing.
     echo "Asking circleci to pick tests based on timings..."
-    export TEST_FILES=$(circleci tests glob "$PWD/test/integration/spec/**/*.js" | circleci tests split --split-by=timings)
+    export TEST_FILES=$PWD/test/integration/spec/localtrackpublication.js
     echo $TEST_FILES
     npm run test:integration
 fi
